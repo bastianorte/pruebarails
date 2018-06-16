8d6c95af98620b3f8f08d6d7496c4fa4fdf3236f
bastian <bastianort@gmail.com>
2018-06-15 21:51:19 -0400

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/todos', to: 'todos#index'
+  get '/todos/new', to: 'todos#new'
+
+  post '/todos', to: 'todos#create'
+  get '/todos/list'
+  get '/todos/:id/edit', to: 'todos#edit', as: 'editar'
+  patch '/todos/:id', to: 'todos#update'
+  delete '/todos/:id', to: 'todos#destroy'
+  get '/todos/:id', to: 'todos#show', as: 'todo'
+  get '/todos/:id/completado', to: 'todos#complete', as: 'completado'
+
+
+end
